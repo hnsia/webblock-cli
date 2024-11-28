a6ee61e89230b0cfe6e9b72af0166b2aca69ccc9
hnsia <44498178+hnsia@users.noreply.github.com>
2024-11-28 20:39:30 +0800

    fix: command config flag docs

--- cmd/root.go
+++ cmd/root.go
@@ -38,7 +38,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is /.config.example.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is /.webblock-cli.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
